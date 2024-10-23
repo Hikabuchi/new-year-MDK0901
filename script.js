a661c6de55cd352ef700361155c9bde555cfaff9
Demchuk Tatiana <133664654+Hikabuchi@users.noreply.github.com>
2024-10-23 10:09:01 +0700

    add

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+    $('#start-button').on('click', function() {
+        startGame();
+    });
 
+    $('#exit-button').on('click', function() {
+        exitGame();
+    });
+
+    function startGame() {
+        // Logic to start the game goes here
+        alert("Game started! Let's play.");
+        // For now, just an alert. You can replace this with your game logic.
+    }
+
+    function exitGame() {
+        if (confirm("Are you sure you want to exit?")) {
+            window.close();
+        }
+    }
+});
