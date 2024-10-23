ae4e30bc7a4521fa49709c06eca05cce1880472c
Demchuk Tatiana <133664654+Hikabuchi@users.noreply.github.com>
2024-10-23 13:29:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
+    const timer = 0;
+    const money = 0;
+    const dishesPrepared = 0;
+
+    let ingredients = ['chocolate', 'dough', 'sugar',
+        'rice', 'fish', 'salt', 'water', 'cocoa powder',
+        'coffee', 'bread', 'cheese', 'sausage', 'lettuce leaves',
+        'carrots', 'butter', 'apple', 'snowflakes', 'ginger',' Tea', 'cider'];
+
+    let dishes = {
+        'cookie' : 'Шоколадное печенье',
+        'fishRice' : 'тарелку  рыбы с рисом',
+        'cocoa' : 'стакан горячего какао',
+        'coffee' : 'стакан горячего кофе',
+        'water':  'стакан воды',
+        'sandwich ': 'бутерброд',
+        'saladCarrots':'салат с морковью',
+        'pie' : 'кусочек пирога',
+        'bowlOfSnowflakes' : 'миску снежинок',
+        'gingerbread': 'имбирный пряник',
+        'tea': 'чашку травяного чая',
+        'cider': ' бокал игристого сидра'
+    };
+
+    let saladWithCarrots = ['Мне, пожалуйста, ', 'Мне ', 'Я ничего не хочу, но возьму ', 'Я с удовольствием съем ', 'Я возьму ', 'Я возьму только '];
+
+    let namesOfTheBuyers = {
+        1:{
+            name : 'Эльф',
+            url : 'путь',
+            alt : 'character'
+        },
+        2:{
+            name : 'Северный олень',
+            url : 'путь',
+            alt : 'character'
+        },
+        3:{
+            name : 'Гринч',
+            url : 'путь',
+            alt : 'character'
+        },
+        4:{
+            name : 'Снеговик Фрости',
+            url : 'путь',
+            alt : 'character'
+        },
+        5:{
+            name : 'Просто олень',
+            url : 'путь',
+            alt : 'character'
+        },
+        6:{
+            name : 'Незнокомец',
+            url : 'путь',
+            alt : 'character'
+        }
+    };
+
 $(document).ready(function() {
-    $('#start-button').on('click', function() {
+    
+    $('.start-button').on('click', function() {
         startGame();
     });
 
@@ -8,14 +68,27 @@ $(document).ready(function() {
     });
 
     function startGame() {
-        // Logic to start the game goes here
-        alert("Game started! Let's play.");
-        // For now, just an alert. You can replace this with your game logic.
+        $('.field-of-play').css('display', 'block');
+        $('.start-window').css('display', 'none');
+        $('.end-window').css('display', 'none');
     }
 
     function exitGame() {
-        if (confirm("Are you sure you want to exit?")) {
-            window.close();
-        }
+        $('.end-window').css('display', 'block');
+        $('.field-of-play').css('display', 'none');
     }
+
+    function recipes() {
+        switch(expression) {
+            case 'chocolate' && 'dough' && 'sugar':
+                dishes.cookie;
+              break;
+            case 'rice' && 'fish' && 'salt':
+                dishes.fishRice;
+              break;
+            default:
+              // code block
+          }
+    }
+    
 });
